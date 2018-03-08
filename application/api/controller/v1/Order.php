c87d66d969226ae20288a8b0c855ba14ebffe06f
supertaozi <supertaozi@gmail.com>
2018-03-08 23:18:52 +0800

    完成订单详情接口

--- application/api/controller/v1/Order.php
+++ application/api/controller/v1/Order.php
@@ -10,30 +10,69 @@ namespace app\api\controller\v1;
 
 
 use app\api\controller\BaseController;
+use app\api\validate\IDMustBePostiveInt;
 use app\api\validate\OrderPlace;
+use app\api\validate\PagingParameter;
 use app\lib\enum\ScopeEnum;
 use app\lib\exception\ForbiddenException;
+use app\lib\exception\OrderException;
 use app\lib\exception\TokenException;
 use think\Controller;
 use app\api\service\Token as TokenService;
 use app\api\service\Order as OrderService;
+use app\api\model\Order as OrderModel;
 
 class Order extends BaseController
 {
     //用户选择商品后，向API提交包含它所选择商品的相关信息
-    //API在接收到信息后，需要检查订单相关
+    //API在接收到信息后，需要检查订单相关商品的库存量
     //有库存，把订单数据存入数据中，等于下单成功了，返回客户端消息，告诉客户端可以支付了
     //调用我们的支付接口，进行支付
     //还需要再次进行库存量检测
     //服务器这边就可以调用微信的支付接口进行支付
-    //微信会返回给我们一个支付的结果
+    //小程序根据服务器返回的结果拉起微信支付
+    //微信会返回给我们一个支付的结果（异步）
     //成功：也需要进行库存量的检查
     //成功：进行库存量的扣除
 
 
     protected $beforeActionList=[
-        'checkExclusiveScope' => ['only' => 'placeOrder']
+        'checkExclusiveScope' => ['only' => 'placeOrder'],
+        'checkPrimaryScope' => ['only' => 'getSummaryByUser'],
     ];
+
+
+    public function getSummaryByUser($page=1, $size=15)
+    {
+        (new PagingParameter())->goCheck();
+        $uid = TokenService::getCurrentUid();
+        $pagingOrders = OrderModel::getSummaryByUser($uid, $page, $size);
+        if($pagingOrders->isEmpty()){
+            return [
+                'data' => [],
+                'current_page' => $pagingOrders->getCurrentPage()
+            ];
+        }
+        $data = $pagingOrders
+            ->hidden(['snap_items','snap_address','prepay_id'])
+            ->toArray();
+        return [
+            'data' => $data,
+            'current_page' => $pagingOrders->getCurrentPage()
+        ];
+    }
+
+    public function getDetail($id)
+    {
+        (new IDMustBePostiveInt())->goCheck();
+        $orderDetail = OrderModel::get($id);
+        if(!$orderDetail)
+        {
+            throw new OrderException();
+        }
+        return $orderDetail
+            ->hidden(['prepay_id']);
+    }
 
     public function placeOrder()
     {
